cbcfa1aab6590f6da3b9c10c27e75c9eb639641b
VikArt217 <vikart217@gmail.com>
2021-12-15 14:34:06 +0300

    add service, slider with responsive

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,23 @@ $(function () {
 		if ($('nav ul').css('display') == 'none') {
 			$('nav ul').slideDown();
 		} else {
-				$('nav ul').slideUp();
+			$('nav ul').slideUp();
 		}
-			
-		});
+	});
+
+	$('.service-slider').slick({
+		arrows: false,
+		dots: true,
+		responsive: [{
+				breakpoint: 700,
+				settings: {
+					dots: false,
+					centerMode: true,
+					centerPadding: '35px'
+				}
+			}
+		]
+
+	});
+
 });
